db0323ff61a69335c1f0f698935551dcb4ac8743
haas <kimsangha617@gmail.com>
2023-05-24 00:52:23 +0900

    POST /api/events returns 201

---
+++ src/main/java/me/haas/demoinflearnrestapi/events/EventRepository.java
@@ -0,0 +1,6 @@
+package me.haas.demoinflearnrestapi.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+}
--- src/test/java/me/haas/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/haas/demoinflearnrestapi/events/EventControllerTests.java
@@ -1,15 +1,22 @@
 package me.haas.demoinflearnrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -25,13 +32,38 @@ public class EventControllerTests {
              //MockMvc 클래스는 요청을 만들수 있고 응답을 검증할수 있는 Spring mvc Test에 있어 가장 핵심적인 클래스
              //MockMvc는 웹서버를 띄우지 않기 때문에 좀 더 빠르지만 DispatcherServlet 을 만들기 때문에 단위 테스트보단 빠르지 않다
 
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     public void createEvent() throws Exception {
+
+        Event event = Event.builder()
+                        .name("Spring")
+                        .beginEnrollmentDateTime(LocalDateTime.of(2023,05,01,16,00))
+                        .closeEnrollmentDateTime(LocalDateTime.of(2023,05,02,16,00))
+                        .beginEventDateTime(LocalDateTime.of(2023,05,05,10,00))
+                        .endEventDateTime(LocalDateTime.of(2023,05,06,00,00))
+                        .basePrice(10000)
+                        .maxPrice(20000)
+                        .limitOfEnrollment(100)
+                        .location("강남 D2 스타터 팩토리")
+                        .build();
+
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
                         .accept(MediaTypes.HAL_JSON) //HyperTextApplicationLanguage
-                        )
-                .andExpect(status().isCreated()); // isCreated() -> 201 response
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated()) // isCreated() -> 201 response
+                .andExpect(jsonPath("id").exists());
     }
 
 
