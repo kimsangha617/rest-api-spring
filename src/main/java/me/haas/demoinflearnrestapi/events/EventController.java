29fef446929a7721f7c365a886876f84ccec4c90
haas <kimsangha617@gmail.com>
2023-06-01 07:57:01 +0900

    validate wrong data

--- src/main/java/me/haas/demoinflearnrestapi/events/EventController.java
+++ src/main/java/me/haas/demoinflearnrestapi/events/EventController.java
@@ -21,15 +21,22 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class EventController {
     private final EventRepository eventRepository;
     private final ModelMapper modelMapper;
-    public EventController(EventRepository eventRepository, ModelMapper modelMapper) {
+    private final EventValidator eventValidator;
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper, EventValidator eventValidator) {
         this.eventRepository = eventRepository;
         this.modelMapper = modelMapper;
+        this.eventValidator = eventValidator;
     }
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
         if(errors.hasErrors()){
             return ResponseEntity.badRequest().build();
         }
+        eventValidator.validate(eventDto, errors);
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+
         Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
--- src/test/java/me/haas/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/haas/demoinflearnrestapi/events/EventControllerTests.java
@@ -88,4 +88,24 @@ public class EventControllerTests {
                         .content(this.objectMapper.writeValueAsString(eventDto)))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2023,05,03,16,00))
+                .closeEnrollmentDateTime(LocalDateTime.of(2023,05,02,16,00))
+                .beginEventDateTime(LocalDateTime.of(2023,05,05,10,00))
+                .endEventDateTime(LocalDateTime.of(2023,05,04,00,00))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남 D2 스타터 팩토리")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
 }
